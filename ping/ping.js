939fd6e4c1837088d58a5751086d2855c2d76e53
root <xet.erixon@gmail.com>
2017-02-15 23:42:47 +0100

    Oled stuff

--- ping/ping.js
+++ ping/ping.js
@@ -1,4 +1,10 @@
 var request = require('request');
+var oled = require('../node-oled/node-oled.js');
+oled.Init().Clear();
+//oled.Invert("on");
+
+
+
 console.log(process.argv.length);
 
 var args = process.argv.splice(2,process.argv.length -2);
@@ -7,20 +13,31 @@ console.log(args);
 var headers = {
     'User-Agent':       'XetPinger/0.0.1'
 }
+var row=0;
+oled.Cursor(row,0).Write("Azure Always On-er").Execute();
+row = row+1;
+oled.Cursor(row,0).Write(args.length + " sites").Execute();
+row = row+1;
 for(i in args){
+ 
     // Configure the request
-    console.log(args[i]);
     var options = {
         url: args[i],
         method: 'GET',
         headers: headers
     }
 
-
+  
   request(options, function (error, response, body) {
-    //console.log(error);
+    console.log(this.host);
     if(!error){
-        console.log(response.statusCode);
+        oled.Cursor(row,0).Write("Ping OK").Execute()
+        //console.log(response.statusCode);
     }
+    else
+    {
+        oled.Cursor(row,0).Write("Ping FAIL").Execute();   
+    }
+    row = row+1;
   });
 }	
